2001ac31aeeb8c93a7a75b966f556cfc028ad127
enis <enisbeygorus1@gmail.com>
2023-01-17 00:09:14 +0300

    cart detail fix

--- src/components/CartUI/CartDetail.tsx
+++ src/components/CartUI/CartDetail.tsx
@@ -1,21 +1,33 @@
 import Button from "../Button/Button";
+import { useSelector } from "react-redux";
+import { ACTION_SELECTORS } from "../../store/actionSelectors";
 
 const CartDetail = () => {
+  const { cartItems } = useSelector(ACTION_SELECTORS.getCart);
+
+  const sumPrice = cartItems
+    .reduce((total, cartItem) => {
+      return total + parseFloat(cartItem.price) * cartItem.amount;
+    }, 0)
+    .toFixed(2);
+
+  const shipmentPrice = "20.00";
+
   return (
     <div className="w-full">
       <div className="border rounded px-4 py-3 mb-4">
         <h3 className="text-left text-xl mb-4">Cart Detail</h3>
         <div className="flex mb-2">
           <span className="flex-1 text-left">Total order</span>
-          <strong>323,99 TL</strong>
+          <strong>{sumPrice} TL</strong>
         </div>
         <div className="flex mb-2">
           <span className="flex-1 text-left">Shipment</span>
-          <strong>20,00 TL</strong>
+          <strong>{shipmentPrice} TL</strong>
         </div>
         <hr />
         <div className="text-right mt-2">
-          <strong>771,99 TL</strong>
+          <strong>{parseFloat(shipmentPrice) + parseFloat(sumPrice)} TL</strong>
         </div>
       </div>
       <div>
