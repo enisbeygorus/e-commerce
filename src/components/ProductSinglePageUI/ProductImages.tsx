68f6994497fa92cba6dc75b79027c198023a4475
enis <enisbeygorus1@gmail.com>
2023-01-24 16:46:41 +0300

    ProductSingle select mini image added

--- src/components/ProductSinglePageUI/ProductImages.tsx
+++ src/components/ProductSinglePageUI/ProductImages.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { IProduct } from "../../types";
 
 type IProductImages = Pick<IProduct, "imageUrls">;
 
 const ProductImages = ({ imageUrls }: IProductImages) => {
+  const [selectedImage, setSelectedImage] = useState<string>(imageUrls[0]);
+
+  const selectImageHandler = (str: string) => {
+    if (str !== selectedImage) {
+      setSelectedImage(str);
+    }
+  };
+
   return (
     <div className="w-full mb-2 flex flex-col md:m-0 md:flex-row flex-1">
       <div className=" flex justify-center order-2 w-full mb-4 px-2 md:block overflow-y-auto md:aspect-square md:w-32 md:order-none">
-        <div className="md:px-2 overflow-auto whitespace-nowrap">
+        <div className="md:px-2 overflow-auto whitespace-nowrap cursor-pointer">
           {imageUrls.map((imageUrl, index) => {
             return (
               <div
+                onClick={() => selectImageHandler(imageUrl)}
                 key={index}
                 className="inline-block w-16 md:w-full mr-4 md:block md:mb-4 md:mr-0"
               >
@@ -25,7 +35,11 @@ const ProductImages = ({ imageUrls }: IProductImages) => {
       </div>
       <div className="flex mb-4 justify-center items-center flex-1 px-4 sm:py-4 md:mb-0">
         <div style={{ aspectRatio: "1 / 1.3" }} className="overflow-hidden">
-          <img alt="product_image_show" className="w-full" src={imageUrls[0]} />
+          <img
+            alt="product_image_show"
+            className="w-full"
+            src={selectedImage}
+          />
         </div>
       </div>
     </div>
